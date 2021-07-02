2b994fe24e93daeb5eb31c7a138e256fe62bba5d
dcartwright2323 <31773100+dcartwright2323@users.noreply.github.com>
2021-07-02 14:21:29 -0400

    completed reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(toReverse) {
+    let toReturn = '';
+    for (let i = toReverse.length - 1; i >= 0; i-- ) {
+        toReturn += toReverse.charAt(i);
+    }
+    return toReturn;
 };
 
 module.exports = reverseString;
